f105b92d74b6a91305f8b0ffe0024a85c79b6db3
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-03-23 14:56:42 -0300

    fazendo funcionar
    Quando começamos esse código, só nós e Deus sabiamos o que ele fazia, agora ..... nada, nós ainda sabemos

--- AritExp.rkt
+++ AritExp.rkt
@@ -10,21 +10,19 @@
 
 (define-peg number (name value (+ (range #\0 #\9))) (string->number value))
 
-(define-peg prod (and (name p1 number) (? (and #\* (name p2 prod))))
-                  (if p2 (prod p1 p2) p1))
 
-(define-peg div (and(name d1 number) (? (and #\/ (name d2 div))))
-                  (if d2 (norm (div d1 d2)) d1)) 
+
 (define (norm k)
                   (match k
                            [(div a (div b c)) (div (div a b) (norm c))]
                            [a a]))
+ ;produto e divisão são operações definidas sobre campos
+(define-peg fieldOp (and (name t1 number) (? (and (name op (or #\* #\/)) (name t2 fieldOp))))
+                  (if t2 (if (equal? op "*") (prod t1 t2) (norm (div t1 t2))) t1 ))
+ 
+;soma e subtração são operações definidas sobre grupos
+(define-peg groupOp (and (name t1 fieldOp) (? (and (name op (or #\+ #\-)) (name t2 groupOp))))
+                  (if t2 (if (equal? op "+") (soma t1 t2) (sub t1 t2)) t1 ))
 
-(define-peg sum (and (name t1 (or prod div)) (? (and #\+ (name t2 sum))))
-                  (if t2 (soma t1 t2) t1 ))
 
-
-(define-peg sub (and (name t1 (or prod div)) (? (and #\- (name t2 sub))))
-                  (if t2 (sub t1 t2) t1 ))
-                  
-(define-peg aritExp (or sum sub))
+(define-peg aritExp groupOp)
