abe828f28cfb548152ccbc86fb08e61c9e47464c
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-03-23 14:26:21 -0300

    Update AritExp.rkt

--- AritExp.rkt
+++ AritExp.rkt
@@ -1,15 +1,30 @@
-#lang typed/racket
+#lang racket
 
 (require peg/peg)
+(struct soma (a b))
+(struct sub (a b))
+(struct prod (a b))
+(struct div (a b))
 
-(struct prod (U Number ([x : Number] [y : prod]))
-(struct div (U Number  ([x : Number] [y: div]))
+
 
 (define-peg number (name value (+ (range #\0 #\9))) (string->number value))
 
-(define-peg prod (and (name p1 number) (? (and #\* (name p2 number)))))
+(define-peg prod (and (name p1 number) (? (and #\* (name p2 prod))))
+                  (if p2 (prod p1 p2) p1))
 
-(define-peg div (and(name d1 number) (?(and #\/ (name d2 number))))) 
+(define-peg div (and(name d1 number) (? (and #\/ (name d2 div))))
+                  (if d2 (norm (div d1 d2)) d1)) 
+(define (norm k)
+                  (match k
+                           [(div a (div b c)) (div (div a b) (norm c))]
+                           [a a]))
+
+(define-peg sum (and (name t1 (or prod div)) (? (and #\+ (name t2 sum))))
+                  (if t2 (soma t1 t2) t1 ))
 
 
-(define-peg aritExp (and (or prod div) (? (and #\+ aritExp))))
+(define-peg sub (and (name t1 (or prod div)) (? (and #\- (name t2 sub))))
+                  (if t2 (sub t1 t2) t1 ))
+                  
+(define-peg aritExp (or sum sub))
