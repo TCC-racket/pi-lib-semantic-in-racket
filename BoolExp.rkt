6358ed221e9b9d176e7fb8a74f24d3a7612baaff
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-03-24 18:55:20 -0300

    Update BoolExp.rkt

--- BoolExp.rkt
+++ BoolExp.rkt
@@ -2,26 +2,18 @@
 
 (require peg/peg)
 
-(struct andOp (a b))
-(struct orOp (a b))
-(struct neg (a))
-(struct eq (a b))
-(struct le (a b))
-(struct be (a b))
-(struct less (a b))
-(struct more (a b))
+(struct andOp (a b)) ;and
+(struct orOp (a b))  ;or
+(struct neg (a))     ;not
+(struct eq (a b))    ;==
+(struct le (a b))    ;<=
+(struct be (a b))    ;>=
+(struct less (a b))  ;<
+(struct more (a b))  ;>
 
 (struct boolExp (U andOp orOp neg eq le be less))
 
 
-;==
-;not
-;>=
-;<=
-;>
-;<
-;or
-;and
 (define-peg boolean (or "true" "false"))
 (define-peg equal (and 
                       (name eq1 (or boolean boolExp)) ; aritExp))
