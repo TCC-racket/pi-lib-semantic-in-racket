234bc20af2ca4aac5d0b26c0af4e02d22c34861f
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-03-24 22:18:42 -0300

    Update BoolExp.rkt

--- BoolExp.rkt
+++ BoolExp.rkt
@@ -16,13 +16,17 @@
 
 (define-peg boolean (or "true" "false"))
 (define-peg equal (and 
-                      (name eq1 (or boolean boolExp)) ; aritExp))
+                      (name eq1 (or boolean)) ; aritExp))
                       "=="
-                      (name eq2 (or boolean boolExp))) ;aritExp)))
+                      (name eq2 (or boolean))) ;aritExp)))
                   (eq eq1 eq2))
-                  
+(define-peg relacional (or equal))
+(define-peg parenteses (and "(" (name value boolExp) ")") value)
 (define-peg negation (and "~" (name n boolExp)) (neg n))
-(define-peg boolExp (or negation equal boolean))
+(define-peg terceiroNivel (or negation parenteses relacional boolean))
+(define-peg conjuncao (and (name con1 terceiroNivel) (? (and "/\" (name con2 conjucao))))   (if con2 (andOp con1 con2) con1))
+(define-peg disjuncao (and (name dis1 conjucao) (? (and "\/" (name dis2 disjuncao))))   (if dis2 (orOp dis1 dis2) dis1))
+(define-peg boolExp disjuncao)
 
 ;boolExp -> ~boolExp | equal | boolean
 ;equal -> boolean==boolExp
