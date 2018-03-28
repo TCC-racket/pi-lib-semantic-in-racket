f71da7d1f092ba4ea11c27bd76591026408b3549
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-03-28 10:14:54 -0300

    Update stack.rkt

--- stack.rkt
+++ stack.rkt
@@ -1,5 +1,7 @@
-(provide (all-from-out))
+#lang racket
+
+(provide (all-defined-out))
 (define empty-stack '())
 (define (push a l) (cons a l))
 (define (stack? l) (list? l))
-(define (pop l) (car l))
+(define (pop l) (values (car l) (cdr l))) ;necessary caution
