b004f2cbc6c8fd3426ce98db715e704a611dea2b
luisFelipeFreitas <luisfreitas@id.uff.br>
2018-03-31 17:46:02 -0300

    Update atribuicao.rkt

--- atribuicao.rkt
+++ atribuicao.rkt
@@ -1,1 +1,11 @@
 
+#lang racket
+
+(require peg/peg)
+(struct atribution(a))
+
+(define-peg variable (and
+		(or(range #\a #\z) (range#\A #\Z))
+		(* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9)))))
+
+(define-peg atribution (and (name t1 variable (? (and ":=" name t2 variable))))	;Provavelmente errado mas vou upar pra ver com vocês essa semana
