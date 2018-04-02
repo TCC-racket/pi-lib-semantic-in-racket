b0e00c1a220acc74651e825a1f4b7248afaebd2f
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-04-02 11:48:44 -0300

    Update condicionais.rkt

--- condicionais.rkt
+++ condicionais.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
+(require peg/peg)
+
 (require "BoolExp.rkt")
 (require "Comando.rkt")
+(require "espacos.rkt")
 (provide peg-rule:condicional)
 
 (struct if (condicao corpo))
@@ -13,7 +16,7 @@
                    "else {" (* (or #\n #\t)) (name corpoElse comando) "}" ) (ifElse condicao corpoIf corpoElse))
 
 (define-peg if (and
-                  "if " (name condicao boolExp) " {\n\t" (name corpo comando) (* #\n) "}") (if condicao corpo))
+                  "if" spaces (name condicao boolExp) spaces "{" newLines tabs (name corpo comando) newLines "}") (if condicao corpo))
 
 (define-peg condicional (or ifElse if))
                   
