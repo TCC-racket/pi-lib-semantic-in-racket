cc185d3a59ffbc8af8313579838d95d4db47fe67
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-08 23:06:16 -0300

    Update Reservadas.rkt

--- Reservadas.rkt
+++ Reservadas.rkt
@@ -3,12 +3,13 @@
 (require peg/peg)
 (provide peg-rule:reserv)
 
-(define-peg var(and "var"))
-(define-peg const(and "const"))
-(define-peg bar(and "|"))
-(define-peg init(and "init"))
-(define-peg proc(and "proc"))
-(define-peg module(and "Module"))
-(define-peg end(and "end"))
-
+(define-peg var "var")
+(define-peg const "const")
+(define-peg bar "|")
+(define-peg init "init")
+(define-peg proc "proc")
+(define-peg module "Module")
+(define-peg end "end")
+(define-peg virg ",")
+ 
 (define-peg reserv (or module end proc var init bar const ))
