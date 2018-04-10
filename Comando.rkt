2ae9c8daa50d33812e2695b3544f8ae6b146d4c2
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-10 20:21:24 -0300

    Add files via upload

--- Comando.rkt
+++ Comando.rkt
@@ -5,7 +5,8 @@
 (require "Reservadas.rkt")
 ;(require "condicionais.rkt")
 ;(require "Exp.rkt")
+;(require "loop.rkt")
 
 (provide peg-rule:comando)
 
-(define-peg comando(or reserv)) ;exp atributions print
+(define-peg comando(or reserv));atribtuions print condicional exp loop
--- atribuicao.rkt
+++ atribuicao.rkt
@@ -1,10 +1,12 @@
 #lang racket
 
 (require peg/peg)
+;(require "BoolExp.rkt")
 
 (provide (all-from-out))
 
 (struct atribution(value var))
+(struct prnt(a))
 
 (define-peg string (and "\"" (* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9))) "\""))
 
@@ -12,7 +14,9 @@
 		(or(range #\a #\z) (range #\A #\Z))
 		(* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9)))))
 
-(define-peg atributions (and (name t1 variable) (or ":=" "=") (name t2 (or boolExp aritExp string))) (atribution t2 t1))
+;(define-peg atributions (and (name t1 variable) (or ":=" "=") (name t2 (or boolExp aritExp string))) (atribution t2 t1))
+
+;(define-peg print(and "print(" (name t1 (or boolExp aritExp variable string)) ")") (prnt t1))
 
 
 
