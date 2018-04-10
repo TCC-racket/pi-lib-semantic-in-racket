e6243cdfd0da513341180fbcfa3350f141934d68
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-04-10 08:57:09 -0300

    Update atribuicao.rkt

--- atribuicao.rkt
+++ atribuicao.rkt
@@ -13,7 +13,7 @@
 		(or(range #\a #\z) (range #\A #\Z))
 		(* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9)))))
 
-;(define-peg atributions (and (name t1 variable) (or ":=" "=") (name t2 (or boolExp aritExp string))) (atribution t2 t1))
+(define-peg atributions (and (name t1 variable) (or ":=" "=") (name t2 (or boolExp aritExp string))) (atribution t2 t1))
 
 
 
