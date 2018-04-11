2218ce62602c990e35d5c3fa8b88a388654b9854
luisFelipeFreitas <luisfreitas@id.uff.br>
2018-04-11 08:01:05 -0300

    Update AritExp.rkt

--- AritExp.rkt
+++ AritExp.rkt
@@ -11,6 +11,10 @@
 (struct parenteses (a))
 (provide peg-rule:aritExp)
 
+(define (reduce f l) (reduceAux f (car l) (cdr l))
+(define (reduceAux f acc l) (if (null? l) acc (reduceAux f (f acc (car l)) (cdr l))
+
+
 (define-peg parenteses (and spaces "(" spaces (name value aritExp) spaces ")" spaces) (parenteses value))
 (define-peg number (name value (+ (range #\0 #\9))) (string->number value))
 (define-peg variable (and 
@@ -18,11 +22,8 @@
                           (* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9)))))
 
 
-(define (norm k)
-                  (match k
-                           [(div a (div b c)) (div (div a b) (norm c))]
-                           [(sub a (sub b c)) (sub (sub a b) (norm c))]
-                           [a a]))
+(define (normSub l) (reduce sub l))
+(define (normDiv l) (reduce div l))
  ;produto e divisão são operações definidas sobre campos
 ;(define-peg fieldOp (and spaces (name t1 (or parenteses number variable)) (? (and spaces (name op (or #\* #\/)) spaces 
 ;                            (name t2 fieldOp))))
@@ -31,10 +32,10 @@
 ;soma e subtração são operações definidas sobre grupos
 ;(define-peg groupOp (and spaces (name t1 fieldOp) (? (and spaces (name op (or #\+ #\-)) spaces (name t2 groupOp))))
 ;                  (if t2 (if (equal? op "+") (soma t1 t2) (norm (sub t1 t2))) t1 ))
-(define-peg divisoes (and (name s1 (or parenteses number variable)) (? (and "/" (name s2 divisoes)))) (norm (div s1 s2)))
-(define-peg produtos (and (name s1 divisoes) (? (and "*" (name s2 produtos)))) (prod s1 s2))
-(define-peg subtracoes (and (name s1 produtos) (? (and "-" (name s2 subtracoes)))) (norm (sub s1 s2)))
-(define-peg somas (and (name s1 subtracoes) (? (and "+" (name s2 somas)))) (soma s1 s2))
+(define-peg divisoes (and (name s1 (or parenteses number variable)) (? (and "/" (name s2 divisoes)))) (cons s1 s2))
+(define-peg produtos (and (name s1 divisoes) (? (and "*" (name s2 produtos)))) (prod (normDiv s1) s2))
+(define-peg subtracoes (and (name s1 produtos) (? (and "-" (name s2 subtracoes)))) (cons s1 s2))
+(define-peg somas (and (name s1 subtracoes) (? (and "+" (name s2 somas)))) (soma (normSub s1) s2))
 
 (define-peg aritExp somas)
 ;(define-peg aritExp groupOp)
