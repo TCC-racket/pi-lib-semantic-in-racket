d742fb3368705f9bfb93321e780afb74e62aa98d
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-10 21:25:25 -0300

    Update atribuicao.rkt

--- atribuicao.rkt
+++ atribuicao.rkt
@@ -3,9 +3,10 @@
 (require peg/peg)
 
 (provide (all-from-out))
+(require "BoolExp.rkt")
+(require "AritExp.rkt")
 
 (struct atribution(value var))
-(struct prnt(a))
 
 (define-peg string (and "\"" (* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9))) "\""))
 
@@ -14,8 +15,3 @@
 		(* (or (range #\a #\z) (range #\A #\Z) (range #\0 #\9)))))
 
 (define-peg atributions (and (name t1 variable) (or ":=" "=") (name t2 (or boolExp aritExp string))) (atribution t2 t1))
-
-(define-peg print(and "print(" (name t1 (or boolExp aritExp variable string)) ")") (prnt t1))
-
-
-
