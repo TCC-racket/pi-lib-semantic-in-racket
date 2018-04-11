6b4a61395f0f11abaf7b2ea6211f643fd978604e
luisFelipeFreitas <luisfreitas@id.uff.br>
2018-04-11 08:29:07 -0300

    Update AritExp.rkt

--- AritExp.rkt
+++ AritExp.rkt
@@ -4,15 +4,15 @@
 
 (require "espacos.rkt")
 
-(struct soma (a b))
-(struct sub (a b))
-(struct prod (a b))
-(struct div (a b))
-(struct parenteses (a))
+(struct soma (a b) #:transparent)
+(struct sub (a b) #:transparent)
+(struct prod (a b) #:transparent)
+(struct div (a b) #:transparent)
+(struct parenteses (a) #:transparent)
 (provide peg-rule:aritExp)
 
-(define (reduce f l) (reduceAux f (car l) (cdr l))
-(define (reduceAux f acc l) (if (null? l) acc (reduceAux f (f acc (car l)) (cdr l))
+(define (reduce f l) (reduceAux f (car l) (cdr l)))
+(define (reduceAux f acc l) (if (null? l) acc (reduceAux f (f acc (car l)) (cdr l))))
 
 
 (define-peg parenteses (and spaces "(" spaces (name value aritExp) spaces ")" spaces) (parenteses value))
@@ -32,10 +32,10 @@
 ;soma e subtração são operações definidas sobre grupos
 ;(define-peg groupOp (and spaces (name t1 fieldOp) (? (and spaces (name op (or #\+ #\-)) spaces (name t2 groupOp))))
 ;                  (if t2 (if (equal? op "+") (soma t1 t2) (norm (sub t1 t2))) t1 ))
-(define-peg divisoes (and (name s1 (or parenteses number variable)) (? (and "/" (name s2 divisoes)))) (cons s1 s2))
-(define-peg produtos (and (name s1 divisoes) (? (and "*" (name s2 produtos)))) (prod (normDiv s1) s2))
-(define-peg subtracoes (and (name s1 produtos) (? (and "-" (name s2 subtracoes)))) (cons s1 s2))
-(define-peg somas (and (name s1 subtracoes) (? (and "+" (name s2 somas)))) (soma (normSub s1) s2))
+(define-peg divisoes (and (name s1 (or parenteses number variable)) (? (and "/" (name s2 divisoes)))) (if s2 (cons s1 s2) (cons s1 '())))
+(define-peg produtos (and (name s1 divisoes) (? (and "*" (name s2 produtos)))) (if s2 (prod (normDiv s1) s2) (normDiv s1)))
+(define-peg subtracoes (and (name s1 produtos) (? (and "-" (name s2 subtracoes)))) (if s2 (cons s1 s2) (cons s1 '())))
+(define-peg somas (and (name s1 subtracoes) (? (and "+" (name s2 somas)))) (if s2 (soma (normSub s1) s2) (normSub s1)))
 
 (define-peg aritExp somas)
 ;(define-peg aritExp groupOp)
