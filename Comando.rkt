402af2419a979604197fbb69b45826b7e2f4af98
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-10 21:29:19 -0300

    Update Comando.rkt

--- Comando.rkt
+++ Comando.rkt
@@ -1,4 +1,12 @@
 #lang racket
 
+(require peg/peg)
+(require "atribuicao.rkt")
+(require "BoolExp.rkt")
+(require "AritExp.rkt")
+
 (struct comando (U seq init atrib print exit))
-(struct seq (comando1 comando2)
+(struct seq (comando1 comando2))
+(struct prnt(a))
+
+(define-peg print(and "print(" (name t1 (or boolExp aritExp variable string)) ")") (prnt t1))
