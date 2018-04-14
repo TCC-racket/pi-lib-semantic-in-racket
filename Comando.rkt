ad18018cf887c13ee8886d990f5dc200c280d4ed
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-14 14:40:10 -0300

    Add files via upload

--- Comando.rkt
+++ Comando.rkt
@@ -13,11 +13,11 @@
 (struct seq (comando1 comando2) #:transparent)
 (struct prnt(a) #:transparent)
 
-(define-peg separador(or virg bar pointvirg newLines))
+(define-peg separador(or virg pointvirg newLines))
 
 (define-peg seq(and (name t1 comando) (?(name sep separador) (name t2 seq))) (if t2 (seq t1 t2) t1))
 
-(define-peg print(and "print(" (name t1 (or boolExp aritExp variable string)) ")") (prnt t1))
+(define-peg print(and "print(" spaces (name t1 (or aritExp variable string boolExp)) spaces ")") (prnt t1))
 
-(define-peg comando(or inicializacao atributions print))
+(define-peg comando(or inicializacao chce atributions print))
 
