8fe812dbb805114f629f47ff35047f7ff4cdc455
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-14 14:49:12 -0300

    Add files via upload

--- BoolExp.rkt
+++ BoolExp.rkt
@@ -28,20 +28,20 @@
                       (name eq2 (or boolean)) spaces) ;aritExp)))
                   (eq eq1 eq2))
 
-(define-peg ncOp (and (name nc1 aritExp) (? (and (name op (or " == " " >= " " <= " " > " " < ")) (name nc2 relacional))))
+(define-peg ncOp (and (name nc1 aritExp) (? (and spaces (name op (or "==" ">=" "<=" ">" "<")) spaces (name nc2 relacional))))
       (if nc2 (if (equal? op " == ") (eq nc1 nc2) 
         (if (equal? op " >= ") (be nc1 nc2) 
           (if (equal? op " <= ") (le nc1 nc2) (if (equal? op " > ") (more nc1 nc2) (less nc1 nc2))))) nc1))
  
 (define-peg relacional (or equal ncOp))
 
-(define-peg parenteses (and "(" (name value boolExp) ")") (parenteses value))
+(define-peg parenteses (and spaces "(" spaces (name value boolExp) spaces ")" spaces) (parenteses value))
 
-(define-peg negation (and "~" (name n boolExp)) (neg n))
+(define-peg negation (and "~"(name n boolExp)) (neg n))
 
 (define-peg terceiroNivel (or negation parenteses relacional boolean))
 
-(define-peg conjuncao (and (name con1 terceiroNivel) (? (and " /\\ " (name con2 conjuncao))))   (if con2 (andOp con1 con2) con1))
+(define-peg conjuncao (and (name con1 terceiroNivel) (? (and spaces "/\\" spaces (name con2 conjuncao))))   (if con2 (andOp con1 con2) con1))
 
 (define-peg disjuncao (and (name dis1 conjuncao) (? (and spaces "\\/" spaces (name dis2 disjuncao))))   (if dis2 (orOp dis1 dis2) dis1))
 
