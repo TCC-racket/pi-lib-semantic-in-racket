10b08cf5f212f847c83a15d865bfea544cf7ced4
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-04-16 11:26:14 -0300

    Update Comando.rkt

--- Comando.rkt
+++ Comando.rkt
@@ -7,17 +7,30 @@
 (require "Reservadas.rkt")
 (require "espacos.rkt")
 
+
 (provide peg-rule:seq)
+(provide peg-rule:choice)
 
 (struct comando (U seq init atrib print exit) #:transparent)
 (struct seq (comando1 comando2) #:transparent)
 (struct prnt(a) #:transparent)
+(struct choice (comando1 comando2) #:transparent)
+
 
 (define-peg separador(or virg pointvirg newLines))
 
-(define-peg seq(and (name t1 comando) (?(name sep separador) (name t2 seq))) (if t2 (seq t1 t2) t1))
+(define-peg seq(and (name t1 cmdUnit) (?(name sep separador) (name t2 seq))) (if t2 (seq t1 t2) t1))
 
 (define-peg print(and "print(" spaces (name t1 (or aritExp variable string boolExp)) spaces ")") (prnt t1))
 
-(define-peg comando(or inicializacao chce atributions print))
+
+(define-peg cmdUnit (or inicializacao atributions print exit))
+
+
+(define-peg comando(or seq choiceOp cmdUnit)
+(define-peg choiceOp (and (name t1 cmdUnit) (? (and bar (name t2 choiceOp)))) (choice t1 t2))
+
+
+
+
 
