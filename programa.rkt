6ed3762e865357a9fd45c9892e44296c5194339a
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-18 18:21:50 -0300

    Update programa.rkt

--- programa.rkt
+++ programa.rkt
@@ -7,7 +7,9 @@
 (require "Reservadas.rkt")
 
 (struct prog (modName Body) #:transparent)
-(struct clauseList (a b) #:transparent)
+(struct variavelOp (a b) #:transparent)
+(struct constanteOp (a b) #:transparent)
+(struct inicializacaoOp (a b) #:transparent)
 (struct proc (procName Body) #:transparent)
 (struct procSeq (proc seq) #:transparent)
 
@@ -20,11 +22,8 @@
 (define-peg token (and
 		(* (or (range #\a #\z) (range #\A #\Z) ))))
 
-(define-peg clauses (and (? (name t1 variavel)) wordSeparator (name t2 clausesAux1)) (if t1 (clauseList t1 t2) t2))
-(define-peg clausesAux1 (and (? (name t1 constante)) wordSeparator (name t2 clausesAux2))(if t1 (clauseList t1 t2) t2))
-(define-peg clausesAux2 (and (? (name t1 inicializacao)) wordSeparator (name t2 seqProcesso))(if t1 (clauseList t1 t2) t2))
+(define-peg variavelCL (and (? (name t1 variavel)) wordSeparator (name t2 clausesAux1)) (if t1 (variavelOp t1 t2) t2))
+(define-peg constanteCL (and (? (name t1 constante)) wordSeparator (name t2 clausesAux2))(if t1 (constanteOp t1 t2) t2))
+(define-peg inicializacaoCL (and (? (name t1 inicializacao)) wordSeparator (name t2 seqProcesso))(if t1 (inicializaOp t1 t2) t2))
 (define-peg seqProcesso (and (name t1 processo) (? wordSeparator (name t2 seqProcesso))) (if t2 (procSeq t1 t2) t1))
 
-;(define-peg clauses (and (? (name t1 variavel)) wordSeparator (? (name t2 constante))
-;                         wordSeparator (? (name t3 inicializacao))
-;                         wordSeparator (+ (name t4 processo))))
