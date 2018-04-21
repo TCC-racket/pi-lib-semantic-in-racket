04e46a4ca41090aac562accb6064c07da98afc1b
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-04-21 10:35:19 -0300

    Update programa.rkt

--- programa.rkt
+++ programa.rkt
@@ -1,3 +1,4 @@
+
 #lang racket
 
 (require peg/peg)
@@ -16,7 +17,7 @@
 (struct procSeq (proc seq) #:transparent)
 
 (define-peg programa (and "(" wordSeparator module wordSeparator (name t1 token)
-                          wordSeparator (name t2 clauses) wordSeparator end wordSeparator ")") (prog t1 t2))
+                          wordSeparator (name t2 variavelCL) wordSeparator end wordSeparator ")") (prog t1 t2))
 
 
 (define-peg processo (and proc wordSeparator (name t1 token) wordSeparator "{" wordSeparator (name t2 comando) wordSeparator "}") (proc t1 t2))
@@ -24,8 +25,7 @@
 (define-peg token (and
 		(* (or (range #\a #\z) (range #\A #\Z) ))))
 
-(define-peg variavelCL (and (? (name t1 variavel)) wordSeparator (name t2 clausesAux1)) (if t1 (variavelOp t1 t2) t2))
-(define-peg constanteCL (and (? (name t1 constante)) wordSeparator (name t2 clausesAux2))(if t1 (constanteOp t1 t2) t2))
-(define-peg inicializacaoCL (and (? (name t1 inicializacao)) wordSeparator (name t2 seqProcesso))(if t1 (inicializaOp t1 t2) t2))
+(define-peg variavelCL (and (? (name t1 variavel)) wordSeparator (name t2 constanteCL)) (if t1 (variavelOp t1 t2) t2))
+(define-peg constanteCL (and (? (name t1 constante)) wordSeparator (name t2 inicializacaoCL))(if t1 (constanteOp t1 t2) t2))
+(define-peg inicializacaoCL (and (? (name t1 inicializacao)) wordSeparator (name t2 seqProcesso))(if t1 (inicializacaoOp t1 t2) t2))
 (define-peg seqProcesso (and (name t1 processo) (? wordSeparator (name t2 seqProcesso))) (if t2 (procSeq t1 t2) t1))
-
