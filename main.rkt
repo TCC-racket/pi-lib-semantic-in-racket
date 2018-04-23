bb9ff2f653644cc820f3acb6d03925e61346f781
petrolifero <joaopedroabreudesouza@cryptolab.net>
2018-04-23 00:17:54 -0300

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -3,13 +3,17 @@
 (require peg/peg)
 (require "programa.rkt")
 
-(define in (open-input-file "C:\\Users\\Pichau\\Desktop\\8 PERÍODO\\COMPILADORES\\RACKET TEST\\fact.imp"))
+;(define in (open-input-file "C:\\Users\\Pichau\\Desktop\\8 PERÍODO\\COMPILADORES\\RACKET TEST\\fact.imp"))
 
-(define l1(lambda()(read-string 1000 in)))
+;(define l1(lambda()(read-string 1000 in)))
 
-(define pgrm(lambda()(peg programa (read-string 1000 in))))
+;(define pgrm(lambda()(peg programa (read-string 1000 in))))
 
 ;(l1) ;o que tem dentro do arquivo
 
-(pgrm) ;peg programa "l1"
+;(pgrm) ;peg programa "l1"
+
+
+(define in (open-input-file (vector-ref (current-comando-line-arguments) 0)))
+(peg programa (port->string in))
 
