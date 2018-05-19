a82623c3170d69cb3ad2d16eb6b486dcf0cce2e8
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-05-18 23:41:01 -0300

    Add files via upload

--- atribuicao.rkt
+++ atribuicao.rkt
@@ -14,15 +14,18 @@
 (provide peg-rule:variavel)
 (provide peg-rule:constante)
 (provide peg-rule:declaracao)
-(provide decSeq)
 
 (struct atribution (var value) #:transparent)
 (struct declaraList (var decList) #:transparent)
 (struct atribSeq (atrib1 atribSeq2) #:transparent)
-(struct decSeq (lista) #:transparent)
+(struct clauses (lista) #:transparent)
 (struct constante (name) #:transparent)
+(struct constanteBlk (iniSeq) #:transparent)
 (struct variavel (name) #:transparent)
+(struct variavelBlk (iniSeq) #:transparent)
 (struct init (name val) #:transparent)
+(struct iniSeq (ini iniSeq) #:transparent)
+(struct decSeq (declaracao decSeq) #:transparent)
 
 (provide atribution)
 
@@ -52,7 +55,21 @@
 (define-peg inicializacao (? (and wordSeparator init wordSeparator (name t1 variable) wordSeparator "=" wordSeparator (name t2 (or boolExp aritExp))
                                (? wordSeparator virg wordSeparator (name t3 declaraINI)))) (cond [t3 (cons (init t1 t2) t3)] [else (init t1 t2)]))
 
-(define-peg declaracao (name t1 (and variavel constante inicializacao))  (decSeq t1))
+;(define-peg clause (name t1 (and variavel constante inicializacao))  (clauses t1))
+
+(define-peg declaracao (or decSeq decUnit))
+
+(define-peg decSeq (and wordSeparator (name t1 decUnit) (? wordSeparator pointvirg wordSeparator (name t2 decSeq))) (cond [t2 (decSeq t1 t2)] [else t1]))
+
+(define-peg decUnit (or constanteBlk variavelBlk))
+
+(define-peg constanteBlk (and wordSeparator const wordSeparator (name t1 iniSeq)) (constanteBlk t1))
+
+(define-peg variavelBlk (and wordSeparator var wordSeparator (name t1 iniSeq)) (variavelBlk t1))
+
+(define-peg inicializacaoBlk (and wordSeparator (name t1 variable) wordSeparator "=" wordSeparator (name t2 (or boolExp aritExp))) (init t1 t2))
+
+(define-peg iniSeq (and wordSeparator (name t1 inicializacaoBlk) (? wordSeparator virg wordSeparator (name t2 iniSeq))) (cond [t2 (iniSeq t1 t2)] [else t1]))
 
 (struct assign (idt exp) #:transparent)
 
