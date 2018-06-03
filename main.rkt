5b026ca874513cfcbae3992558cdf7ab4c1ec6b1
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-06-03 00:20:28 -0300

    adicionado alguns testes na main
    falta transformar esses testes em suites de testes do rackunit

--- main.rkt
+++ main.rkt
@@ -22,22 +22,55 @@
 (module+ test
   (require peg/peg)
 (require rackunit)
-(executeSMC (comandoConv (peg comando "x := 5 ;
-    y := 1 ;
-		while ~ (x == 0)
-        do {               
-		  y := y * x ;
-          x := x - 1
-		} ; 
-		print(y);
-	}"))) )
-;  (executeSMC
- ;   (comandoConv (peg comando "x := 5 ;
- ;   y := 1 ;
-;		while ~ (x == 0)
- ;       do {               
-;		  y := y * x ;
- ;         x := x - 1
-;		} ; 
-;		print(y)
-;	}"))))
+
+(executeSMC (comandoConv (peg comando "
+if (true)
+{
+	var x := 0, y:= 8, z := 12;
+}
+"))) ; deve retornar um ambiente vazio, uma memoria vazia e tudo mais vazio
+
+(executeSMC (comandoConv (peg comando "
+if (true)
+{
+	print(1);
+}
+"))) ; deve retornar a mesma coisa do anterior
+
+(executeSMC (comandoConv (peg comando "
+if (true)
+{
+	var x := 0, y:= 8, z := 12;
+	const a:= x, x := 23;
+}
+"))) ; deve retornar um erro pois x esta sendo declarado duas vezes
+
+(executeSMC (comandoConv (peg comando "
+if (true)
+{
+	var x := 0;
+	if(true)
+	{
+		var x := 1;
+		print(x);
+	}
+	print(x);
+}
+"))) ; deve printar 1 e depois 0
+
+(executeSMC (comandoConv (peg comando "
+if (true)
+{
+	var x := 0;
+	if(true)
+		var x:=1;
+		print(x);
+	print(x);
+}
+"))) ; deve printar 0 duas vezes, pois sem chaves, o primeiro print esta fora do if mais interno
+
+
+
+
+
+)
