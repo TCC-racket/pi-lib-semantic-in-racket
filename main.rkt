266dc80868b2e9fe9856226e804371f0b6537c6b
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-06-04 12:33:03 -0300

    Os testes na main.rkt estão no padrão rackunit
    Cada programa esta englobado numa suite de testes, a qual sequencialmente
    verifica o resultado do parser, da tradução do bplc e do smc.
    
    TODO: adicionar mais testes, notadamente o mutex e multiplas declarações
    iguais para ver a tradução do bplc levantando exceções.

--- main.rkt
+++ main.rkt
@@ -20,57 +20,122 @@
 
 
 (module+ test
-  (require peg/peg)
-(require rackunit)
-
-(executeSMC (comandoConv (peg comando "
-if (true)
-{
-	var x := 0, y:= 8, z := 12;
-}
-"))) ; deve retornar um ambiente vazio, uma memoria vazia e tudo mais vazio
-
-(executeSMC (comandoConv (peg comando "
-if (true)
-{
-	print(1);
-}
-"))) ; deve retornar a mesma coisa do anterior
-
-(executeSMC (comandoConv (peg comando "
-if (true)
-{
-	var x := 0, y:= 8, z := 12;
-	const a:= x, x := 23;
-}
-"))) ; deve retornar um erro pois x esta sendo declarado duas vezes
-
-(executeSMC (comandoConv (peg comando "
-if (true)
-{
-	var x := 0;
-	if(true)
-	{
-		var x := 1;
-		print(x);
-	}
-	print(x);
-}
-"))) ; deve printar 1 e depois 0
-
-(executeSMC (comandoConv (peg comando "
-if (true)
-{
-	var x := 0;
-	if(true)
-		var x:=1;
-		print(x);
-	print(x);
-}
-"))) ; deve printar 0 duas vezes, pois sem chaves, o primeiro print esta fora do if mais interno
+	(require peg/peg)
+	(require rackunit)
 
 
+	(test-suite "if com chaves, com declarações, sem comandos"
+
+		(define saidaParser (peg comando "
+			if (true)
+			{
+				var x := 0, y:= 8, z := 12;
+			}"))
+		
+		(check-equal? 
+			saidaParser
+			()) ;;Raphael, o que o parser deveria retornar aqui??
+		(define saidaBPLC (comandoConv saidaParser))
+		
+		(check-equal?
+			saidaBPLC
+			()) ;;Luis, o que a tradução pro bplc deveria ser??
+		(define saidaSMC (executeSMC saidaBPLC))
+
+		(check-equal?
+			saidaSMC
+			())) ;;João, o que o smc deveria devolver??
+	(test-suite "if com chaves, sem declarações, com comandos"
+
+		(define saidaParser (peg comando "
+			if (true)
+			{
+				print(1);
+			}"))
+		
+		(check-equal? 
+			saidaParser
+			()) ;;Raphael, o que o parser deveria retornar aqui??
+		(define saidaBPLC (comandoConv saidaParser))
+		
+		(check-equal?
+			saidaBPLC
+			()) ;;Luis, o que a tradução pro bplc deveria ser??
+		(define saidaSMC (executeSMC saidaBPLC))
+
+		(check-equal?
+			saidaSMC
+			())) ;;João, o que o smc deveria devolver??
+
+	(test-suite "if com chaves, com multiplas declarações, sem comandos"
+		(define saidaParser (peg comando "
+			if (true)
+			{
+				var x := 0, y:= 8, z := 12;
+				const a:= x, x := 23;
+			}
+		"))
+		(check-equal? 
+			saidaParser
+			()) ;;Raphael, o que o parser deveria retornar aqui??
+		(define saidaBPLC (comandoConv saidaParser))
+		
+		(check-equal?
+			saidaBPLC
+			()) ;;Luis, o que a tradução pro bplc deveria ser??
+		(define saidaSMC (executeSMC saidaBPLC))
+
+		(check-equal?
+			saidaSMC
+			())) ;;João, o que o smc deveria devolver??
+	(test-suite "if com chaves, com uma declaração, com if interno, com chaves, com declaração e comando, seguido por comando"
+		(define saidaParser (peg comando "
+			if (true)
+			{
+				var x := 0;
+				if(true)
+				{
+					var x := 1;
+					print(x);
+				}
+				print(x);
+			}"))
+		(check-equal? 
+			saidaParser
+			()) ;;Raphael, o que o parser deveria retornar aqui??
+		(define saidaBPLC (comandoConv saidaParser))
+		
+		(check-equal?
+			saidaBPLC
+			()) ;;Luis, o que a tradução pro bplc deveria ser??
+		(define saidaSMC (executeSMC saidaBPLC))
+
+		(check-equal?
+			saidaSMC
+			())) ;;João, o que o smc deveria devolver??
 
 
+	(test-suite "if com chaves, com uma declaração,com um if interno, sem chaves com declaração,seguido por  comando e outro comando"
+		(define saidaParser (peg comando "
+			if (true)
+			{
+				var x := 0;
+				if(true)
+					var x:=1;
+					print(x);
+				print(x);
+			}"))
+		(check-equal? 
+			saidaParser
+			()) ;;Raphael, o que o parser deveria retornar aqui??
+		(define saidaBPLC (comandoConv saidaParser))
+		
+		(check-equal?
+			saidaBPLC
+			()) ;;Luis, o que a tradução pro bplc deveria ser??
+		(define saidaSMC (executeSMC saidaBPLC))
 
-)
+		(check-equal?
+			saidaSMC
+			())) ;;João, o que o smc deveria devolver??
+
