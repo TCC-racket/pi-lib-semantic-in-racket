98c8bdcf5db52936ca2440a0e104803170c011f9
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-12-24 02:31:03 -0200

    nada

--- parser.rkt
+++ parser.rkt
@@ -2,8 +2,8 @@
 
 _ < [ \t\n]* ;
 EOI < ! . ;
-top <- v:file EOI -> v;
-file <- _ v1:module _ v2:command _ -> (blk v1 v2);
+top <- _ v:file _ EOI -> v;
+file <- v1:module _ v2:command -> (blk v1 v2);
 module <- 'module' _ identifier _ v1:globalVariable _ v2:proc _ v3:fun _ v4:gen-> (dec v1 (dec v2 (dec v3 v4)));
 identifier <- v:[a-zA-Z]+ -> (idt v);
 globalVariable <- 
